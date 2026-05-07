18794f5c0e172c79c7a484f334b8c85ad7e68086
Darko <dark.araya@duocuc.cl>
2026-05-07 19:10:34 -0400

    feat: agregué validaciones en User(Service)

--- user-service/user-service/src/main/java/com/cine/user_service/model/User.java
+++ user-service/user-service/src/main/java/com/cine/user_service/model/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,12 +21,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
+    @Email(message = "Correo inválido")
+    @NotBlank(message = "El correo es obligatorio")
     private String correo;
 
+    @NotBlank(message = "La contraseña es obligatoria")
     private String contraseña;
-
+    
+    @NotBlank(message = "El rol es obligatorio")
     private String rol;
 
 }
